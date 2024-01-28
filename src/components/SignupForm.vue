689b6d912026b160441aaefa251f67751a198c8b
Felipe Palacio <felipepalaciobetancourt@gmail.com>
2024-01-28 17:45:50 +0100

    Add Change Add #

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -15,7 +15,7 @@
     <label>Skills:</label>
     <input type="text" v-model="tempSkill" @keyup="addSkill" />
     <div v-for="skill in skills" :key="skill" class="pill">
-      {{ skill }}
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
     </div>
 
     <div class="terms">
@@ -69,6 +69,11 @@ export default {
         this.tempSkill = '';
       }
     },
+    deleteSkill(skill) {
+      this.skills = this.skills.filter((item) => {
+        return skill != item;
+      });
+    },
   },
 };
 </script>
@@ -110,5 +115,18 @@ input[type='checkbox'] {
   margin: 0 10px 0 0;
   position: relative;
   top: 2px;
+}
+
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
 }
 </style>
