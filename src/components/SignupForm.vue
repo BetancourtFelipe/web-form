bf70b2fad33fc5f36c22cf519a547b492a89612a
Felipe Palacio <felipepalaciobetancourt@gmail.com>
2024-01-28 16:08:08 +0100

    Add skills

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -12,6 +12,12 @@
       <option value="designer">Web Designer</option>
     </select>
 
+    <label>Skills:</label>
+    <input type="text" v-model="tempSkill" @keyup="addSkill" />
+    <div v-for="skill in skills" :key="skill" class="pill">
+      {{ skill }}
+    </div>
+
     <div class="terms">
       <input type="checkbox" required v-model="terms" />
       <label>Accept terms and conditions</label>
@@ -37,6 +43,7 @@
   <p>Password: {{ password }}</p>
   <p>Role {{ role }}</p>
   <p>Terms {{ terms }}</p>
+  <p>Skills {{ tempSkill }}</p>
   <!-- <p>names: {{ names }}</p> -->
 </template>
 
@@ -48,8 +55,20 @@ export default {
       password: '',
       role: '',
       terms: false,
+      tempSkill: '',
+      skills: [],
       // names: [],
     };
+  },
+  methods: {
+    addSkill(e) {
+      if (e.key === ',' && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill);
+        }
+        this.tempSkill = '';
+      }
+    },
   },
 };
 </script>
