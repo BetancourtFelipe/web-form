cbfb9a51055604d52382ec1791913ea5d59117b9
Felipe Palacio <felipepalaciobetancourt@gmail.com>
2024-01-28 18:14:30 +0100

    Add handleSubmit

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,10 +1,11 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Email:</label>
     <input type="email" v-model="email" />
 
     <label>Password:</label>
-    <input type="password" v-model="password" />
+    <input type="password" required v-model="password" />
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
     <label>Role:</label>
     <select v-model="role">
@@ -21,6 +22,10 @@
     <div class="terms">
       <input type="checkbox" required v-model="terms" />
       <label>Accept terms and conditions</label>
+    </div>
+
+    <div class="submit">
+      <button>Create an Account</button>
     </div>
 
     <!-- <div>
@@ -57,6 +62,7 @@ export default {
       terms: false,
       tempSkill: '',
       skills: [],
+      passwordError: '',
       // names: [],
     };
   },
@@ -73,6 +79,21 @@ export default {
       this.skills = this.skills.filter((item) => {
         return skill != item;
       });
+    },
+    handleSubmit() {
+      // validate password
+      this.passwordError =
+        this.password.length > 5
+          ? ''
+          : 'Password must be at least 6 chars long';
+
+      if (!this.passwordError) {
+        console.log('email: ', this.email);
+        console.log('password: ', this.password);
+        console.log('role: ', this.role);
+        console.log('skills: ', this.skills);
+        console.log('terms accepted: ', this.terms);
+      }
     },
   },
 };
@@ -128,5 +149,25 @@ input[type='checkbox'] {
   font-weight: bold;
   color: #777;
   cursor: pointer;
+}
+
+button {
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+
+.submit {
+  text-align: center;
+}
+
+.error {
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
 }
 </style>
